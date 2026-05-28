21d53cfa2cadc48e367e019419667674bd394717
Christian Alexander Camacho <rfcamacris@gmail.com>
2026-05-27 22:52:33 -0500

    ssaass

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function NumeroPresente(Valor) {
 }
 
 function ActualizarDisplay(Valor) {
-  document.getElementById("display").value = Valor;
+  document.getElementById("displaaay").value = Valor;
 }
 
 function OperadorFun(Operacion) {
